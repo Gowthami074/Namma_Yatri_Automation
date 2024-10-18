84cfcf927fe222b482cf7894f80014564e2003ce
Sumedh-14 <sumedhkrishna7@gmail.com>
2024-10-18 15:31:14 +0530

    Segreggated Driver side PopUps and Added Loc Permission for A14

--- movingTech.NY/Driver/DriverLoginFlow.java
+++ movingTech.NY/Driver/DriverLoginFlow.java
@@ -15,7 +15,7 @@ import base.BaseClass;
 import io.appium.java_client.AppiumBy;
 
 public class DriverLoginFlow extends BaseClass{
-
+    PopUpsHandling popUpsHandling = new PopUpsHandling();
 	@Test
 	public void successfulDriverLogin() throws InterruptedException {
 		System.out.println("Driver login begins");
@@ -68,75 +68,11 @@ public class DriverLoginFlow extends BaseClass{
 
 		//		driver.findElements(AppiumBy.xpath("//android.widget.TextView[@text='Maybe Later']"));
 		//		driver.findElements(AppiumBy.xpath("//android.widget.TextView[@text='GO!']"));
-
-		pointsAndUpiPopsHandling();
-		driverModeValidation();		
+		popUpsHandling.locationPermissionPopUp();
+		popUpsHandling.upiOverlayPopUp();
+		popUpsHandling.yatriCoinsOverlayPopUp();
+		popUpsHandling.pointsOverLay();
+		popUpsHandling.driverModeValidation();		
 	}
-
-
-	
-	public void pointsAndUpiPopsHandling() {
-		// Temporarily reduce implicit wait before using explicit wait
-		implicitWaitMethod(driver,5); //This method is defined in BaseClass
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5)); // Set a small explicit wait time
-		
-		try {
-			// Wait for 'Later' button (UPI Overlay)
-			WebElement laterButton = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text='Later']")));
-			System.out.println("Skipping UPI Overlay");
-			laterButton.click();
-
-
-		} catch (TimeoutException e) {
-			System.out.println("No UPI Overlay found");
-		}
-		try {
-			// Wait for 'Later' button (UPI Overlay)
-			WebElement yatriCoinsOkayButton = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text='Okay']")));
-			System.out.println("Skipping UPI Overlay");
-			yatriCoinsOkayButton.click();
-
-
-		} catch (TimeoutException e) {
-			System.out.println("No Yatricoins Overlay found");
-		}
-		try {
-			// Wait for 'Maybe Later' button (Points Overlay)
-			WebElement maybeLaterButton = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text='Maybe Later']")));
-			System.out.println("Skipping Points Overlay");
-			maybeLaterButton.click();
-		} catch (TimeoutException e) {
-			System.out.println("No Points Overlay found");
-		}
-		// Temporarily reduce implicit wait before using explicit wait
-		implicitWaitMethod(driver,60);
-	}
-
-
-	public void driverModeValidation() throws InterruptedException {
-		implicitWaitMethod(driver,5);
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5)); // Adjust the timeout as needed
-
-		try {
-			// Wait for 'GO!' button to appear (Driver is offline)
-			WebElement goButton = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text='GO!']")));
-			System.out.println("The driver is offline");
-			goButton.click();
-
-		} catch (TimeoutException e1) {
-			try {
-				// Wait for 'Silent' button (Driver is silent)
-				WebElement silentButton = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text='Silent']/../android.widget.ImageView")));
-				System.out.println("The Driver is Silent");
-
-				// Now wait for 'Online' button and click it
-				WebElement onlineButton = wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text='Online']")));
-				onlineButton.click();
-
-			} catch (TimeoutException e2) {
-				System.out.println("The driver is online");
-			}
-			implicitWaitMethod(driver,60);
-		}
-	}}
+}
 
--- movingTech.NY/Driver/EndRideScreen.java
+++ movingTech.NY/Driver/EndRideScreen.java
@@ -7,17 +7,23 @@ import io.appium.java_client.AppiumBy;
 
 public class EndRideScreen extends BaseClass {
 	
+	PopUpsHandling popUpsHandling = new PopUpsHandling();
 	@Test
 	public void clickEndRide() throws InterruptedException {
+		System.out.println("Checking Whether Points Overlay is displaying");
+		popUpsHandling.pointsOverLay();
 		System.out.println("Coming to tap on the EndRide");
 		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='End Ride']")).click();
 		System.out.println("Waiting to tap on the Endride in the pop up");
 		Thread.sleep(3000);
 		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='End Ride : Button']")).click();
 		Thread.sleep(3000);
+		popUpsHandling.takeUnlimitedRidesPopUp();
 		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Fare Collected']")).click();
 		Thread.sleep(2000);
 	}
+	
+	
 	
 	public void clickMaps() { 
 		
