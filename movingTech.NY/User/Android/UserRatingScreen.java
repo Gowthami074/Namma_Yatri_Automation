4344b990fd5e200ec378cd8668670f031dd683c1
Gowthami Allu <gowthami.allu@Gowthami-Allu-C02D5HZSMD6M.local>
2025-01-27 12:59:15 +0530

    rideflow

--- movingTech.NY/User/Android/UserRatingScreen.java
+++ movingTech.NY/User/Android/UserRatingScreen.java
@@ -7,6 +7,7 @@ import base.BaseClass;
 import io.appium.java_client.AppiumBy;
 
 public class UserRatingScreen extends BaseClass {
+	PopUpsHandling popup= new PopUpsHandling();
 	
 	@Test
 	public void TollRideRatingScreen() throws InterruptedException{
@@ -81,4 +82,42 @@ public class UserRatingScreen extends BaseClass {
         }
         System.out.println("Exiting UserRatingScreen method");
     }
+    @Test
+    public void ratingScreen2() throws InterruptedException {
+    	popup.AC_PopUp();
+    	String rideFare = driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Ride completed']/../android.view.ViewGroup[1]/android.widget.TextView")).getText();
+		System.out.println("Ride Completed With "+rideFare);
+		
+		driver1.findElement(AppiumBy.xpath("//android.widget.Button[@content-desc='View ride details']")).click();
+		
+		String vehicleModel = driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Ride Time']/../android.widget.TextView[3]")).getText();
+		String driverName = driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Ride Time']/../android.widget.TextView[4]")).getText();
+		String rideDistance = driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Ride Time']/../android.widget.TextView[7]")).getText();
+		String rideTime = driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Ride Time']/../android.widget.TextView[8]")).getText();
+		String rideId = driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Ride Time']/../android.widget.TextView[11]")).getText();
+		
+		System.out.println("DATA FROM THE BOOKING DETAILS:: VehicleModel: "+vehicleModel+"Driver Name: "+driverName+"Ride Distance: "+rideDistance+"Ride Time: "+rideTime+"Ride Id: "+rideId);
+		
+		driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='View Driver Receipt']")).click();
+		System.out.println("Driver Receipt Opend");
+		String totalPaidAmount = driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Total Paid']/../android.widget.TextView[2]")).getText();
+		String tripTime = driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Total Paid']/../android.widget.TextView[3]")).getText();
+		
+		System.out.println("Details From Receipt:: Total Paid Ammount: "+totalPaidAmount+"Trip Time: "+tripTime);
+		
+		driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Download PDF']")).click();
+		System.out.println("Driver Receipt downloaded successfully");
+		Thread.sleep(7000);
+		driver1.findElement(AppiumBy.xpath("//com.horcrux.svg.SvgView[@content-desc='Go back']")).click();
+		
+		Thread.sleep(2000);
+		driver1.findElement(AppiumBy.xpath("//com.horcrux.svg.SvgView[@content-desc='Go back']")).click();
+		
+		driver1.findElement(AppiumBy.xpath("//android.widget.ImageButton[@content-desc='5 Star']")).click();
+		System.out.println("5 start rating given");
+		driver1.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Submit & Continue']")).click();
+		Thread.sleep(2000);
+		
+		
+	}
 }
