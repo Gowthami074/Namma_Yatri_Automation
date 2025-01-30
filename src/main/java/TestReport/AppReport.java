6a40c5071dda2325e0922dbe331119ad66bf215d
Vinit Joshi <vinitjoshi@Vinits-MacBook-Pro.local>
2025-01-30 17:45:39 +0530

    Fix project setup and enabling script execution by cmd

---
+++ src/main/java/TestReport/AppReport.java
@@ -0,0 +1,34 @@
+package TestReport;
+
+import java.io.File;
+import java.io.IOException;
+
+public class AppReport {
+
+	public void generateReport() {
+		new Thread(() -> {
+			try {
+				System.out.println("Serving Allure Report...");
+				String command = "/opt/homebrew/bin/allure serve allure-results --clean";
+				Runtime.getRuntime().exec(command);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.err.println("Failed to serve Allure report.");
+			}
+		}).start();
+	}
+
+	public void clearExistingJSONFiles() {
+		System.out.println("Cleaning allure-results directory...");
+		File resultsDir = new File("allure-results"); // Path to your allure-results directory
+		if (resultsDir.exists()) {
+			for (File file : resultsDir.listFiles()) {
+				if (!file.isDirectory()) {
+					file.delete(); // Delete files
+				}
+			}
+		}
+		System.out.println("allure-results directory cleaned.");
+	}
+
+}
